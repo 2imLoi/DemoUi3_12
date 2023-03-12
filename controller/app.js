c2ec860948edb2d33eb3eb4a5ecc2f0fc84e7b77
AlexNguyen <nguyenhoainam121n@gmail.com>
2023-03-12 22:09:54 +0700

    FE our-menu: -m index.html  -a our-menu.html, -a our-menu.css

--- controller/app.js
+++ controller/app.js
@@ -1,7 +1,12 @@
-const restaurantApp = angular.module("restaurantApp", ["ngRoute"]);
+const restaurantApp = angular.module('restaurantApp', ['ngRoute'])
 restaurantApp.config(function ($routeProvider) {
-  $routeProvider.when("/", {
-    templateUrl: "/views/component/home/index.html",
-    controller: "homeCtrl",
-  });
-});
+	$routeProvider
+		.when('/', {
+			templateUrl: '/views/component/home/index.html',
+			controller: 'homeCtrl',
+		})
+		.when('/our-menu', {
+			templateUrl: '/views/component/our-menu/our-menu.html',
+			controller: 'ourMenuCtrl',
+		})
+})
